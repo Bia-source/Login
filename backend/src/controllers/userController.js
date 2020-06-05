10a7f86bfef41cd11fdf2dcd5be57dfd527228a0
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-06-05 11:25:04 -0300

    adicionando try catch na função de registrar

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,8 +2,13 @@ const User = require('../models/user');
 
 module.exports = {
     async registrar(req,res) {
-        const user = await User.create(req.body);
-        return res.json(user);
+        try{
+            const user = await User.create(req.body);
+            return res.json(user);
+        } catch (err){
+           return res.status(400).send({ error: 'Falha no registro'});
+        }
+        
     }
 
 }
